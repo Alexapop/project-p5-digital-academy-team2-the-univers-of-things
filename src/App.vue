3f3f6e20b25b545cde8839681cba2c360cbc36c9
AndreaVaGo <andrea.vallina.glez@gmail.com>
2026-06-15 10:11:42 +0200

    fix: integrate login button in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,16 @@
 <script setup>
 import { RouterView } from 'vue-router'
 import HeaderComponent from './components/header/HeaderComponent.vue'
+import LoginButton from './components/login/LoginButton.vue'
+
 </script>
 
 <template>
-  <HeaderComponent />
+  <header>
+    <HeaderComponent />
+    <LoginButton /> 
+  </header>
+  
   <RouterView />
 </template>
 
