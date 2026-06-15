3837f492d5b4e72d6d78dde49ba46c1fffa03f8b
AndreaVaGo <andrea.vallina.glez@gmail.com>
2026-06-15 15:08:49 +0200

    feat: add and integrate pagination component at the bottom of HomeView

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <script setup>
 
 import PaginationHub from '../components/pagination/PaginationHub.vue'
-import LoginButton from '../components/login/LoginButton.vue'
+
 
 
 </script>
@@ -9,7 +9,7 @@ import LoginButton from '../components/login/LoginButton.vue'
 <template>
 
   <h1>The Univers of Things</h1>
-  <LoginButton />
+
 
   <main>
     <PaginationHub />
