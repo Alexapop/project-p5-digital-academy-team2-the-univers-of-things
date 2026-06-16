1160695535a671f58ded0fef7b50a7ee228288b9
unknown <avilarranz@gmail.com>
2026-06-16 10:11:18 +0200

    feat:created anime card component

---
+++ src/views/DestacadosView.vue
@@ -0,0 +1,14 @@
+<script setup>
+import AnimeCard from '@/components/card/AnimeCard.vue';
+
+
+</script>
+
+<template>
+
+  <h1>The Univers of Things</h1>
+  <AnimeCard />
+
+  <main>
+  </main>
+</template>
