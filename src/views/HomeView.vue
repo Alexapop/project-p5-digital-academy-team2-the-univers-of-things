7a3c241f334e966f2c7afff3fd04c2224b736e36
AndreaVaGo <andrea.vallina.glez@gmail.com>
2026-06-17 13:41:44 +0200

    fix: update import paths using src alias and restore layout

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,10 @@
 <script setup>
-import HeroSection from '../components/hero/HeroSection.vue'
-import PaginationHub from '../components/pagination/PaginationHub.vue'
+import HeroSection from '@/components/hero/HeroSection.vue'
+import PaginationHub from '@/components/pagination/PaginationHub.vue'
 import HomeCatalogSearchInput from "@/components/home-catalog/HomeCatalogSearchInput.vue"
 import HomeCatalogGenreFilter from "@/components/home-catalog/HomeCatalogGenreFilter.vue"
-import HeaderComponent from '../components/header/HeaderComponent.vue'
-import FooterComponent from '../components/footer/FooterComponent.vue'
+import HeaderComponent from '@/components/header/HeaderComponent.vue'
+import FooterComponent from '@/components/footer/FooterComponent.vue'
 </script>
 
 <template>
